e5a97818bf16377f992b377b39adf32362581ba8
Xuan Li <mowangblog@qq.com>
2021-10-27 22:03:12 +0800

    update HelloWorld

---
+++ RabbitMQ-HelloWorld/src/main/java/top/mowang/mq7/ReceiveLogsTopic02.java
@@ -0,0 +1,33 @@
+package top.mowang.mq7;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+import top.mowang.util.RabbitMQUtil;
+
+/**
+ * RabbitMQ-Demo
+ *
+ * @author : Xuan Li
+ * @website : https://mowangblog.top
+ * @date : 2021/10/27 21:45
+ **/
+public class ReceiveLogsTopic02 {
+    private static final String EXCHANGE_NAME = "topic_logs";
+
+    public static void main(String[] args) throws Exception {
+        Channel channel = RabbitMQUtil.getChannel();
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+        //声明队列
+        String queueName = "Q2";
+        channel.queueDeclare(queueName,false,false,false,null);
+        channel.queueBind(queueName,EXCHANGE_NAME,"*.*.rabbit");
+        channel.queueBind(queueName,EXCHANGE_NAME,"lazy.#");
+        System.out.println("等待接收消息,把接收到的消息打印在屏幕........... ");
+        DeliverCallback deliverCallback = (consumerTag, delivery) ->
+        {String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println("ReceiveLogsTopic02控制台打印接收到的消息"+message+"绑定:"+delivery.getEnvelope().getRoutingKey());
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {});
+    }
+}
